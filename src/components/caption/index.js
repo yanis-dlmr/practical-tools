5dd3ceba1d99a54e6a4924d3540fd165207f6ae8
yanis-dlmr <yanisdeladraw@gmail.com>
2023-10-01 01:59:57 +0200

    .bmp, .jpg, .jpeg, .png

--- src/components/caption/index.js
+++ src/components/caption/index.js
@@ -4,6 +4,7 @@ class Caption {
         this.caption = document.createElement("div");
         this.caption.textContent = title;
         this.caption.setAttribute("class", "h4"); 
+        this.caption.setAttribute("margin-bottom", "10px")
     }
 
     render() {
--- src/components/importer/index.js
+++ src/components/importer/index.js
@@ -5,8 +5,9 @@ class Importer {
             this.fileInput = document.createElement('input');
             this.fileInput.classList.add('form-control');
             this.fileInput.type = 'file';
-            this.fileInput.accept = '.BMP';
+            this.fileInput.accept = '.BMP, .jpg, .jpeg, .png';
             this.fileInput.classList.add('mb-3');
+            this.fileInput.multiple = true;
         }
     }
 
