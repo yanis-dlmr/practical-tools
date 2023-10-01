3f913061e850aac0142e84a07f132009d0a2a85a
yanis-dlmr <yanisdeladraw@gmail.com>
2023-10-01 11:04:51 +0200

    opencv import cdn

--- src/html/pictures.php
+++ src/html/pictures.php
@@ -11,5 +11,6 @@
     </body>
 
     <script type="module" src="/src/includes/pictures/index.js"></script>
+    <script src=" https://cdn.jsdelivr.net/npm/opencv@7.0.0/lib/opencv.min.js "></script>
 
 </html>
