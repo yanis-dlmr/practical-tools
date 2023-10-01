5fb1dbbb04674eeea70e5c4e23a021fe6bd710b8
yanis-dlmr <yanisdeladraw@gmail.com>
2023-10-01 15:49:29 +0200

    Update index.js

--- src/includes/pictures/index.js
+++ src/includes/pictures/index.js
@@ -120,7 +120,7 @@ class PictureManager {
     average_pictures = () => {
         var picture_avg = this.cv_pictures[0].clone();
         for (let i = 0; i < this.cv_pictures.length; i++) {
-            picture = this.cv_pictures[i];
+            var picture = this.cv_pictures[i];
             cv.cvtColor(picture_avg, picture, cv.COLOR_RGB2GRAY);
         }
         cv.imshow('canvasOutputBlock', picture_avg);
