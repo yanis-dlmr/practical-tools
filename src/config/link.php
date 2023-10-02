e860ce740bbda41cd8d113cd192724df9381ba53
yanis-dlmr <yanisdeladraw@gmail.com>
2023-10-02 17:35:28 +0200

    boost css

--- src/config/link.php
+++ src/config/link.php
@@ -1,2 +1,3 @@
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 <link rel="stylesheet" href="/src/css/style.css">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"></link>
