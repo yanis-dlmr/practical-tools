3b0940827fb7c50e9e3e1abc41be5dfe664b9d3f
yanis-dlmr <yanisdeladraw@gmail.com>
2023-10-02 13:39:28 +0200

    colors

--- src/html/home.php
+++ src/html/home.php
@@ -10,11 +10,11 @@
     <body>
 
         <div class="container container-sm" style="margin-top: 100px; max-width: 700px;">
-            <div class="card" style="background-color: #252529;">
+            <div class="card">
                 <h3 class="card-header text-center card-title">
                     Welcome !
                 </h3>
-                <div class="card-body" style="color: #fff;">
+                <div class="card-body">
                     <p>This website was developed to offer processing and visualization tools as part of the practical work of the Energy and Propulsion department</p>
                     <p>Enjoy !</p>
                     <p class='text-center'><img src="/src/assets/images/logo.ico" style="height:250px;"></p>
