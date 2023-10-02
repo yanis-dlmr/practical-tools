fd75e6b4f7ca9cc20e246a78f7a70cc863e801b7
yanis-dlmr <yanisdeladraw@gmail.com>
2023-10-02 13:48:11 +0200

    home test

--- src/html/home.php
+++ src/html/home.php
@@ -8,19 +8,8 @@
     </head>
 
     <body>
-
-        <div class="container container-sm" style="margin-top: 100px; max-width: 700px;">
-            <div class="card">
-                <h3 class="card-header text-center card-title">
-                    Welcome !
-                </h3>
-                <div class="card-body">
-                    <p>This website was developed to offer processing and visualization tools as part of the practical work of the Energy and Propulsion department</p>
-                    <p>Enjoy !</p>
-                    <p class='text-center'><img src="/src/assets/images/logo.ico" style="height:250px;"></p>
-                </div>
-            </div>
-        </div>
     </body>
+
+    <script type="module" src="/src/includes/home/index.js"></script>
 
 </html>
---
+++ src/includes/home/index.js
@@ -0,0 +1,76 @@
+document.addEventListener('DOMContentLoaded', function() {
+    init_home();
+});
+
+async function init_home() {
+
+    const cardContainer = document.createElement('div');
+    cardContainer.classList.add('card');
+    cardContainer.style.backgroundColor = 'transparent';
+    cardContainer.style.maxWidth = '60%'
+    cardContainer.style.margin = '0 auto'; 
+    cardContainer.style.boxShadow = '0 0.5rem 1rem rgba(0,0,0,0.15)';
+    cardContainer.style.marginTop = '5rem';
+    cardContainer.style.marginBottom = '5rem';
+
+    const cardHeader = document.createElement('div');
+    cardHeader.classList.add('card-header');
+    cardHeader.id = 'header';
+    cardHeader.setAttribute('style', 'text-align: center; font-size: 2.5rem; font-weight: bold; color: #2061ae; border-radius: 0.25rem; padding: 0.5rem 1rem; margin-bottom: 1rem;')
+    cardHeader.textContent = "Practicals Tools"
+
+    const cardBody = document.createElement('div');
+    // put cardBody1 and cardBody2 next to each other
+    cardBody.style.display = 'flex';
+    cardBody.style.flexDirection = 'row';
+    cardBody.style.flexWrap = 'wrap';
+    cardBody.style.justifyContent = 'space-between';
+    cardBody.style.alignItems = 'center';
+    cardBody.style.textAlign = 'center';
+    cardBody.style.margin = '0 auto';
+    cardBody.style.marginBottom = '1rem';
+    cardBody.style.padding = '0.5rem 1rem';
+
+    const cardBody1 = document.createElement('div');
+    cardBody1.classList.add('card-body');
+    cardBody1.id = 'bloc';
+    cardBody1.style.width = '70%';
+    cardBody1.innerHTML = 
+`<h4> Welcome ! </h4>
+This site contains tools developed in order to help students during their practicals in the department Energetic and Propulsion.
+
+<h4> The website is divided into 1 categories for the moment: </h4>
+• A tool to handle <a class="custom-link" href="/pictures">Pictures</a> and do treatments on them 
+
+<h4> Disclaimer </h4>
+The tools are still in development, some features may not work properly. If you encounter any problems, please contact us.
+`
+//cardBody2 next to cardBody1
+    const cardBody2 = document.createElement('div');
+    cardBody2.classList.add('card-body');
+    cardBody2.id = 'bloc';
+    cardBody2.style.width = '30%';
+    //picture of the logo
+    cardBody2.innerHTML =
+    `
+<img src="/assets/images/logo.ico" alt="logo" style="width: 100%; height: auto; margin-top: 1rem; margin-bottom: 1rem;">
+    `   
+
+
+    const cardFooter = document.createElement('div');
+    cardFooter.classList.add('card-footer');
+    cardFooter.id = 'footer';
+    cardFooter.setAttribute('style', 'text-align: center; font-size: 1rem; color: #2061ae; border-radius: 0.25rem; padding: 0.5rem 1rem; margin-top: 1rem;')
+    cardFooter.textContent = 
+    `
+Please take a look at the documentation before using the tools and to have further informations.
+    `
+
+    cardContainer.appendChild(cardHeader);
+    cardBody.appendChild(cardBody1);
+    cardBody.appendChild(cardBody2);
+    cardContainer.appendChild(cardBody);
+    cardContainer.appendChild(cardFooter);
+
+    document.body.appendChild(cardContainer);
+}
