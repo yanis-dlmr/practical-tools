80d1b5981d6ee40257596a426f1c4f145c252152
yanis-dlmr <yanisdeladraw@gmail.com>
2023-10-02 17:20:48 +0200

    input required

--- src/components/importer/index.js
+++ src/components/importer/index.js
@@ -6,6 +6,7 @@ class Importer {
             this.fileInput.classList.add('form-control');
             this.fileInput.type = 'file';
             this.fileInput.id = importer_id;
+            this.fileInput.required = true;
             this.fileInput.accept = '.BMP, .jpg, .jpeg, .png';
             this.fileInput.classList.add('mb-3');
             this.fileInput.multiple = true;
