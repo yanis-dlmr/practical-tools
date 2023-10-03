5bea3adaf681fe2c0398da0d23555988c8bcb93b
yanis-dlmr <yanisdeladraw@gmail.com>
2023-10-03 22:49:54 +0200

    ChartJs importation

--- src/utils/head.php
+++ src/utils/head.php
@@ -6,6 +6,7 @@
 <?php include '../config/menu.php'; ?>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.8.0/styles/github-dark.min.css">
 
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/4.4.0/chart.min.js" integrity="sha512-7U4rRB8aGAHGVad3u2jiC7GA5/1YhQcQjxKeaVms/bT66i3LVBMRcBI9KwABNWnxOSwulkuSXxZLGuyfvo7V1A==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
 <script src="https://unpkg.com/highlightjs-copy@1.0.3/dist/highlightjs-copy.min.js"></script>
 <link rel="stylesheet" href="https://unpkg.com/highlightjs-copy@1.0.3/dist/highlightjs-copy.min.css" />
