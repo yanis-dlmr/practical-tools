721587eca47f5f9ba13f64ec7ee288afdcb9bc05
yanis-dlmr <yanisdeladraw@gmail.com>
2023-10-03 20:53:20 +0200

    custom-pre

--- src/components/embeded_block/index.js
+++ src/components/embeded_block/index.js
@@ -7,6 +7,7 @@ class EmbededBlock {
     render() { // render embeded block using highlight.js
         const pre = document.createElement('pre');
         pre.setAttribute('style', 'tab-size: 4;');
+        pre.setAttribute('class', 'custom-pre');
         const code = document.createElement('code');
         code.setAttribute('class', 'language-javascript');
         code.innerHTML = this.text;
