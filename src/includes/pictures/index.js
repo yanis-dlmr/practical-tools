11b43a4cc88c7b2f71dfbdbcbe9be695d3c192e5
yanis-dlmr <yanisdeladraw@gmail.com>
2023-10-04 10:16:26 +0200

    Update index.js

--- src/includes/pictures/index.js
+++ src/includes/pictures/index.js
@@ -699,6 +699,7 @@ class PictureManager {
         hljs.highlightAll();
 
         // Rerender MathJax-script
+        MathJax.Hub.Queue(["Typeset", MathJax.Hub]);
         MathJax.typeset();
         MathJax.typesetPromise();
         
