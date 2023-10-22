223494fd57b1f80acca9dc8381d39578e7c3ee38
yanis-dlmr <yanisdeladraw@gmail.com>
2023-10-22 16:54:25 +0200

    NACA test

---
+++ src/html/naca.php
@@ -0,0 +1,16 @@
+<?php include '../utils/start.php'; ?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Practical Tools | NACA profiles</title>
+        <?php include '../utils/head.php'; ?>
+    </head>
+
+    <body>
+        <?php include '../utils/header.php'; ?>
+    </body>
+
+    <script type="module" src="/src/includes/naca/index.js"></script>
+
+</html>
---
+++ src/includes/naca/index.js
@@ -0,0 +1,49 @@
+import { Card } from '/src/components/card/index.js';
+import { Container } from '/src/components/container/index.js';
+import { Caption } from '/src/components/caption/index.js';
+import { Button } from '/src/components/button/index.js';
+import { Select } from '/src/components/select/index.js';
+import { Form } from '/src/components/form/index.js';
+import { EmbededBlock } from '/src/components/embeded_block/index.js';
+import { ChartJs } from '/src/components/chartjs/index.js';
+
+
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    new NacaManager();
+});
+
+class NacaManager {
+
+    constructor() {
+
+        this.create_body();
+
+    }
+
+    create_body() {
+        
+        const row = document.createElement('div');
+        row.classList.add('row', 'custom-row');
+        document.body.appendChild(row);
+
+        // NACA settings
+        const card = new Card('NACA Parameters');
+        const cardElement = card.render();
+
+        const container = new Container('12', 'center');
+        container.addComponent(cardElement);
+        const containerElement = container.render();
+
+        row.appendChild(containerElement);
+
+        const form = new Form();
+        const formElement = form.render();
+        cardElement.addComponent(formElement);
+        
+        form.add_caption('NACA Profile');
+    }
+}
+
