3c023154f0774e356483c542049059106c0bc9fd
yanis-dlmr <yanisdeladraw@gmail.com>
2023-10-25 12:14:33 +0200

    Update naca.js

--- src/includes/naca/naca.js
+++ src/includes/naca/naca.js
@@ -102,7 +102,6 @@ class NACA {
             let angle = this.theta[i];
             let theta = angle * Math.PI / 180;
             let dz_dx = this.dyc_dx_theta[i];
-            console.log (dz_dx);
             integral += dz_dx;
         }
         alpha0 = integral / Math.PI;
