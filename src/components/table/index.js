c1f789d277f9797f4bea977395e040340200c004
yanis-dlmr <yanisdeladraw@gmail.com>
2023-10-26 16:45:18 +0200

    Update index.js

--- src/components/table/index.js
+++ src/components/table/index.js
@@ -7,7 +7,7 @@ class Table {
 
     render() {
         const table = document.createElement('table');
-        table.classList.add('table', 'table-responsive', 'table-hover', 'table-light');
+        table.classList.add('table', 'table-responsive', 'table-hover', 'table-light', 'table-bordered');
 
         const thead = document.createElement('thead');
         const tr = document.createElement('tr');
@@ -36,7 +36,7 @@ class Table {
 
     render_left_headers() { // put headers on the left side of the table instead of the top
         const table = document.createElement('table');
-        table.classList.add('table', 'table-responsive', 'table-hover', 'table-light');
+        table.classList.add('table', 'table-responsive', 'table-hover', 'table-light', 'table-bordered');
 
         const thead = document.createElement('thead');
         let i = 0;
@@ -45,10 +45,10 @@ class Table {
         th.innerHTML = this.headers[i];
         tr.appendChild(th);
         for (let j = 0; j < this.data[i].length; j++) {
-            const td = document.createElement('td');
+            const th = document.createElement('th');
             //rounded data
-            td.innerHTML = Math.round(this.data[i][j] * 100) / 100;
-            tr.appendChild(td);
+            th.innerHTML = Math.round(this.data[i][j] * 100) / 100;
+            tr.appendChild(th);
         }
         thead.appendChild(tr);
         table.appendChild(thead);
