8b5b236d2d482e476c39762caf5c5f380c3af042
yanis-dlmr <yanisdeladraw@gmail.com>
2023-10-31 17:28:15 +0100

    Revert "half profil points"
    This reverts commit 1d0a153814368b9efee26d77b1f75cbeba0a0545.

--- src/includes/airfoil/naca.js
+++ src/includes/airfoil/naca.js
@@ -42,7 +42,7 @@ class NACA {
 
     generate_naca_profile() {
         // Generate x range from 0 to chord with 100 points
-        this.x = Array.from(Array(51).keys()).map(x => x * this.naca_chord / 100);
+        this.x = Array.from(Array(101).keys()).map(x => x * this.naca_chord / 100);
 
         // Generate yc (camber line)
         const x_origin = this.naca_chord / 2;
